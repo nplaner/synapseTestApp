35bfb046a6db39f3211bd46c249ac8f7bb426650
Hang Xu <hangxu199371@gmail.com>
2020-06-16 11:28:13 -0400

    just before pulling

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import styles from "./styles.css";
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+ReactDOM.render(<h1>Helloqweqwe, world!</h1>, document.getElementById("root"));
